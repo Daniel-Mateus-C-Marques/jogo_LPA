d389ccb3a0c20663a18ae17accbee18ae8e361dd
Daniel Marques <danielmateus631@gmail.com>
2025-03-02 17:30:04 +0000

    Creating menu items, sound and background

---
+++ code/const.py
@@ -0,0 +1,10 @@
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+COLLOR_ORANGE = (255, 128, 0)
+COLLOR_WHITE = (255, 255, 255)
+
+MENU_OPTION = ('NEW GAME',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import COLLOR_ORANGE, WIN_WIDTH, MENU_OPTION, COLLOR_WHITE
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./assets/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer.music.load('./assets/Menu.mp3')
+        pygame.mixer.music.play(-1)
+        while True:
+            self.window.blit(self.surf, self.rect)
+            self.menu_text(50, 'Mountain', COLLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', COLLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLLOR_WHITE, ((WIN_WIDTH / 2), 180 + 30 * i))
+
+            pygame.display.flip()
+            # for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
